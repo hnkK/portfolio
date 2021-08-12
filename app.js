c414cbb3c1136369cce9ef3c10435ff669203e7e
Henkes <henkeskevin@outlook.com>
2021-08-12 16:33:51 +0200

    Light mode function implementation

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const navbar = document.querySelector("#navbar");
 const sideMenu = document.querySelector("#side_menu");
+const body = document.querySelector("body");
 
-console.log(sideMenu);
+console.log(body);
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-    const { scrollTop, scrollHeight, clientHeight} =
-    document.documentElement;
-
-    if (scrollTop > 50) {
-        navbar.classList.add("nav_transition_in");
-        navbar.classList.remove("nav_transition_out")
-    } else {
-        navbar.classList.remove("nav_transition_in");
-        navbar.classList.add("nav_transition_out")
-    }
-})
+  if (scrollTop > 50) {
+    navbar.classList.add("nav_transition_in");
+    navbar.classList.remove("nav_transition_out");
+  } else {
+    navbar.classList.remove("nav_transition_in");
+    navbar.classList.add("nav_transition_out");
+  }
+});
 
 function hide() {
-    sideMenu.classList.add("hide")
-    console.log("xd")
+  sideMenu.classList.add("hide");
+  console.log("xd");
+}
+
+function switchTheme() {
+  body.classList.toggle("light_theme");
 }
